ee2a8af463744ff59ec3cb885fcf019c42f9198f
Szymon <szymon.morawski13@wp.pl>
2025-10-03 13:05:33 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace GastroWaga
+{
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "Błąd (nieobsłużony)", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true; // nie zamykaj aplikacji
+        }
+    }
+}
